6b4ef4f6b926e951e3a7b60e178e9671f9b4e385
Sveta <svglaz@yahoo.ca>
2022-05-18 01:21:08 -0700

    Login, logout and create new student methods are added.

--- wegostudy_tests.py
+++ wegostudy_tests.py
@@ -8,5 +8,7 @@ class WeGoStudyPositiveTestCases(unittest.TestCase):
     @staticmethod  # signal to unittest that this is a function inside class (vs @classmethod)
     def test_WeGoStudy():  # test_ in the name is mandatory
         methods.setUp()
-
+        methods.log_in()
+        methods.create_new_student()
+        methods.log_out()
         methods.tearDown()
